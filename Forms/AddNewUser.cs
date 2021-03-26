0f7ed5eae839c7624d0416696c55eb54724bc566
raulrotar <raul_rotar@yahoo.com>
2021-03-26 16:25:22 +0200

    button new user changed

--- Forms/AddNewUser.cs
+++ Forms/AddNewUser.cs
@@ -19,7 +19,7 @@ namespace ProiectII.Forms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Totul e ok!");
+            MessageBox.Show("Modificat butonul de raul!");
         }
     }
 }
