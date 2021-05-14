7ce71bc93b664acd13e295de82d19ed54c94c887
lupastean-iulian <ilupastean@yahoo.com>
2021-05-14 10:25:44 +0300

    Visualize All Visits UC Final Front End

--- UserControls/UC_VisualizeAllVisits.Designer.cs
+++ UserControls/UC_VisualizeAllVisits.Designer.cs
@@ -37,6 +37,7 @@ namespace ProiectII.UserControls
             this.txtBox_Search = new System.Windows.Forms.TextBox();
             this.comboBox_SearchBy = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -49,7 +50,7 @@ namespace ProiectII.UserControls
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1630, 115);
+            this.panel1.Size = new System.Drawing.Size(1630, 150);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -57,32 +58,35 @@ namespace ProiectII.UserControls
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(574, 35);
+            this.label1.Location = new System.Drawing.Point(639, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(485, 41);
+            this.label1.Size = new System.Drawing.Size(308, 41);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Visualize All Visits Ever Made";
+            this.label1.Text = "Visualize All Visits";
             // 
             // btn_ClearSearchFields
             // 
-            this.btn_ClearSearchFields.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btn_ClearSearchFields.BackColor = System.Drawing.Color.DarkCyan;
             this.btn_ClearSearchFields.FlatAppearance.BorderSize = 0;
             this.btn_ClearSearchFields.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_ClearSearchFields.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_ClearSearchFields.ForeColor = System.Drawing.Color.White;
-            this.btn_ClearSearchFields.Location = new System.Drawing.Point(678, 708);
+            this.btn_ClearSearchFields.Location = new System.Drawing.Point(680, 864);
             this.btn_ClearSearchFields.Name = "btn_ClearSearchFields";
             this.btn_ClearSearchFields.Size = new System.Drawing.Size(267, 91);
             this.btn_ClearSearchFields.TabIndex = 12;
             this.btn_ClearSearchFields.Text = "Clear Fields";
             this.btn_ClearSearchFields.UseVisualStyleBackColor = false;
+            this.btn_ClearSearchFields.Click += new System.EventHandler(this.btn_ClearSearchFields_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Silver;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(176, 254);
+            this.dataGridView1.Location = new System.Drawing.Point(178, 376);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1325, 403);
+            this.dataGridView1.Size = new System.Drawing.Size(1325, 437);
             this.dataGridView1.TabIndex = 11;
             // 
             // btn_Search
@@ -92,7 +96,7 @@ namespace ProiectII.UserControls
             this.btn_Search.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Search.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Search.ForeColor = System.Drawing.Color.White;
-            this.btn_Search.Location = new System.Drawing.Point(1206, 146);
+            this.btn_Search.Location = new System.Drawing.Point(1228, 217);
             this.btn_Search.Name = "btn_Search";
             this.btn_Search.Size = new System.Drawing.Size(229, 71);
             this.btn_Search.TabIndex = 10;
@@ -101,19 +105,30 @@ namespace ProiectII.UserControls
             // 
             // txtBox_Search
             // 
+            this.txtBox_Search.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtBox_Search.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtBox_Search.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.txtBox_Search.Location = new System.Drawing.Point(809, 163);
+            this.txtBox_Search.Location = new System.Drawing.Point(797, 237);
             this.txtBox_Search.Name = "txtBox_Search";
-            this.txtBox_Search.Size = new System.Drawing.Size(338, 35);
+            this.txtBox_Search.Size = new System.Drawing.Size(376, 28);
             this.txtBox_Search.TabIndex = 9;
+            this.txtBox_Search.Text = "Search Here";
+            this.txtBox_Search.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBox_Search.Click += new System.EventHandler(this.VAV_Click);
+            this.txtBox_Search.Enter += new System.EventHandler(this.VAV_Enter);
+            this.txtBox_Search.Leave += new System.EventHandler(this.VAV_Leave);
             // 
             // comboBox_SearchBy
             // 
+            this.comboBox_SearchBy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.comboBox_SearchBy.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox_SearchBy.ForeColor = System.Drawing.Color.DodgerBlue;
             this.comboBox_SearchBy.FormattingEnabled = true;
-            this.comboBox_SearchBy.Location = new System.Drawing.Point(383, 163);
+            this.comboBox_SearchBy.Items.AddRange(new object[] {
+            "Day",
+            "Month",
+            "Year"});
+            this.comboBox_SearchBy.Location = new System.Drawing.Point(369, 232);
             this.comboBox_SearchBy.Name = "comboBox_SearchBy";
             this.comboBox_SearchBy.Size = new System.Drawing.Size(310, 38);
             this.comboBox_SearchBy.TabIndex = 8;
@@ -123,17 +138,26 @@ namespace ProiectII.UserControls
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label2.Location = new System.Drawing.Point(222, 166);
+            this.label2.Location = new System.Drawing.Point(230, 237);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(133, 30);
             this.label2.TabIndex = 7;
             this.label2.Text = "Search By:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel2.Location = new System.Drawing.Point(797, 270);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(376, 1);
+            this.panel2.TabIndex = 13;
             // 
             // UC_VisualizeAllVisits
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.btn_ClearSearchFields);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btn_Search);
@@ -141,9 +165,10 @@ namespace ProiectII.UserControls
             this.Controls.Add(this.comboBox_SearchBy);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "UC_VisualizeAllVisits";
-            this.Size = new System.Drawing.Size(1630, 850);
+            this.Size = new System.Drawing.Size(1630, 1000);
+            this.Load += new System.EventHandler(this.UC_VisualizeAllVisits_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -162,5 +187,6 @@ namespace ProiectII.UserControls
         private System.Windows.Forms.TextBox txtBox_Search;
         private System.Windows.Forms.ComboBox comboBox_SearchBy;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
     }
 }
